d327e91a08778c2bf7b0cf37f939bd94b84af06c
Yung-Luen Lan <yllan@me.com>
2021-01-18 14:21:11 +0800

    Initial Commit

---
+++ JXLook/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  JXLook
+//
+//  Created by Yung-Luen Lan on 2021/1/18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JXLook/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JXLook
+//
+//  Created by Yung-Luen Lan on 2021/1/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
