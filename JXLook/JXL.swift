yllan/JXLook

commit d036b8caa3cf44936dbf774d939db3165f8e4d57

Support grey-scale image and update libjxl to 0.3.7+

JXLook/JXL.swift
@@ -57,6 +57,9 @@ struct JXL {
                         Swift.print("Cannot get basic info")
                         break parsingLoop
                     }
+                    if infoPtr.pointee.num_color_channels == 1 {
+                        format = JxlPixelFormat(num_channels: 1, data_type: infoPtr.pointee.bits_per_sample == 16 ? JXL_TYPE_UINT16 : JXL_TYPE_UINT8, endianness: JXL_NATIVE_ENDIAN, align: 0)
+                    }
                     Swift.print("basic info: \(infoPtr.pointee)")
                 case JXL_DEC_SUCCESS:
                     return true
@@ -73,16 +76,30 @@ struct JXL {
                     
                 case JXL_DEC_FULL_IMAGE:
                     let info = infoPtr.pointee
-                    let colorSpace = icc.flatMap({ NSColorSpace(iccProfileData: Data(buffer: $0)) }) ?? .sRGB
-                    if let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(info.xsize), pixelsHigh: Int(info.ysize), bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false, colorSpaceName: .calibratedRGB, bytesPerRow: 4 * Int(info.xsize), bitsPerPixel: 32)?.retagging(with: colorSpace) {
-                        imageRep.size = CGSize(width: Int(info.xsize) / 2, height: Int(info.ysize) / 2)
-                        if let pixels = imageRep.bitmapData {
-                            memmove(pixels, buffer.baseAddress, buffer.count)
+                    if info.num_color_channels == 1 { // greyscale
+                        let colorSpace: NSColorSpace = .genericGray
+                        if let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(info.xsize), pixelsHigh: Int(info.ysize), bitsPerSample: Int(info.bits_per_sample), samplesPerPixel: 1, hasAlpha: false, isPlanar: false, colorSpaceName: .calibratedWhite, bytesPerRow: Int(info.bits_per_sample) / 8 * Int(info.xsize), bitsPerPixel: Int(info.bits_per_sample))?.retagging(with: colorSpace) {
+                            imageRep.size = CGSize(width: Int(info.xsize), height: Int(info.ysize))
+                            if let pixels = imageRep.bitmapData {
+                                memmove(pixels, buffer.baseAddress, buffer.count)
+                            }
+                            let img = NSImage(size: imageRep.size)
+                            img.addRepresentation(imageRep)
+                            image = img
                         }
-                        let img = NSImage(size: imageRep.size)
-                        img.addRepresentation(imageRep)
-                        image = img
+                    } else { // assume it's rgb
+                        let colorSpace = icc.flatMap({ NSColorSpace(iccProfileData: Data(buffer: $0)) }) ?? .sRGB
+                        if let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(info.xsize), pixelsHigh: Int(info.ysize), bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false, colorSpaceName: .calibratedRGB, bytesPerRow: 4 * Int(info.xsize), bitsPerPixel: 32)?.retagging(with: colorSpace) {
+                            imageRep.size = CGSize(width: Int(info.xsize), height: Int(info.ysize))
+                            if let pixels = imageRep.bitmapData {
+                                memmove(pixels, buffer.baseAddress, buffer.count)
+                            }
+                            let img = NSImage(size: imageRep.size)
+                            img.addRepresentation(imageRep)
+                            image = img
+                        }
                     }
+
                     
                 case JXL_DEC_NEED_IMAGE_OUT_BUFFER:
                     var outputBufferSize: Int = 0

.gitignore
@@ -1,3 +1,6 @@
+
+libjxl
+
 # Xcode
 #
 # gitignore contributors: remember to update Global/Xcode.gitignore, Objective-C.gitignore & Swift.gitignore

.gitmodules
@@ -1,3 +1,3 @@
 [submodule "jpeg-xl"]
 	path = jpeg-xl
-	url = https://gitlab.com/yllan_tw/jpeg-xl.git
+	url = https://github.com/libjxl/libjxl.git

JXLook/Info.plist
@@ -38,7 +38,7 @@
 	<key>CFBundleShortVersionString</key>
 	<string>$(APP_VERSION)</string>
 	<key>CFBundleVersion</key>
-	<string>26ad786</string>
+	<string>1036</string>
 	<key>LSApplicationCategoryType</key>
 	<string>public.app-category.photography</string>
 	<key>LSMinimumSystemVersion</key>

JXLookTests/Info.plist
@@ -17,6 +17,6 @@
 	<key>CFBundleShortVersionString</key>
 	<string>1.0</string>
 	<key>CFBundleVersion</key>
-	<string>26ad786</string>
+	<string>1036</string>
 </dict>
 </plist>

JXLookUITests/Info.plist
@@ -17,6 +17,6 @@
 	<key>CFBundleShortVersionString</key>
 	<string>1.0</string>
 	<key>CFBundleVersion</key>
-	<string>26ad786</string>
+	<string>1036</string>
 </dict>
 </plist>

JXQuickLook/Info.plist
@@ -19,7 +19,7 @@
 	<key>CFBundleShortVersionString</key>
 	<string>$(APP_VERSION)</string>
 	<key>CFBundleVersion</key>
-	<string>26ad786</string>
+	<string>1036</string>
 	<key>LSMinimumSystemVersion</key>
 	<string>$(MACOSX_DEPLOYMENT_TARGET)</string>
 	<key>NSExtension</key>

commit b67a1b790e40cfcfadafade49bbcdc7b256fe918

fix: let CG determines imageRep size
Changes in d036b8caa3cf44936dbf774d939db3165f8e4d57 introduced a regression that image is not rendered as pixel size in HiDPI media. This commit removes size setting

JXLook/JXL.swift
@@ -94,7 +94,6 @@ struct JXL {
                             bitmapFormat |= NSBitmapImageRep.Format.alphaNonpremultiplied.rawValue
                         }
                         if let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(info.xsize), pixelsHigh: Int(info.ysize), bitsPerSample: Int(info.bits_per_sample), samplesPerPixel: num_channels, hasAlpha: info.alpha_bits != 0, isPlanar: false, colorSpaceName: .calibratedWhite, bitmapFormat: .init(rawValue: bitmapFormat), bytesPerRow: Int(info.bits_per_sample) / 8 * num_channels * Int(info.xsize), bitsPerPixel: Int(info.bits_per_sample) * num_channels)?.retagging(with: colorSpace) {
-                            imageRep.size = CGSize(width: Int(info.xsize), height: Int(info.ysize))
                             if let pixels = imageRep.bitmapData {
                                 memmove(pixels, buffer.baseAddress, buffer.count)
                             }
@@ -111,7 +110,6 @@ struct JXL {
                             bitmapFormat |= NSBitmapImageRep.Format.alphaNonpremultiplied.rawValue
                         }
                         if let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(info.xsize), pixelsHigh: Int(info.ysize), bitsPerSample: 32, samplesPerPixel: num_channels, hasAlpha: info.alpha_bits != 0, isPlanar: false, colorSpaceName: colorSpaceName, bitmapFormat: .init(rawValue: bitmapFormat), bytesPerRow: 4 * num_channels * Int(info.xsize), bitsPerPixel: 32 * num_channels)?.retagging(with: colorSpace) {
-                            imageRep.size = CGSize(width: Int(info.xsize), height: Int(info.ysize))
                             if let pixels = imageRep.bitmapData {
                                 memmove(pixels, buffer.baseAddress, buffer.count)
                             }